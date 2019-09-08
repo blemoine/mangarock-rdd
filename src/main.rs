ef99e85ec72293f68fef053db96949bda1b1278a
Benoit Lemoine <blemoine@xebia.fr>
2019-09-08 09:28:47 -0400

    fix: display rss even when there is 1 error

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,28 @@ use mangarockclient::MangaError;
 
 impl actix_web::error::ResponseError for MangaError {}
 
-fn index(_req: &HttpRequest) -> Result<String, MangaError>  {
+fn index(_req: &HttpRequest) -> Result<String, MangaError> {
     let oids: Vec<MangaOid> = vec![
         "mrs-serie-288364", // 3rei,
         "mrs-serie-26314",  // berserk
         "mrs-serie-303939", //the promised neverland
         "mrs-serie-295440", //shingeki no kyojin
+        "mrs-serie-132938", //Kimetsu no yaiba
     ]
         .iter()
         .map(|s| MangaOid::new(s))
         .collect();
 
-    let infos = oids.iter().map(|oid| info_on(oid)).collect::<Result<Vec<MangaInfo>, MangaError>>()?;
+
+    let infos = oids.iter().filter_map(|oid| {
+        match info_on(oid) {
+            Ok(info) => Some(info),
+            Err(err) => {
+                println!("Got an error for manga {}: {}", oid, err);
+                None
+            }
+        }
+    }).collect();
 
     Ok(rss_for(&infos, Utc::now()))
 }
